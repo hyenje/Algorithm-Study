b0d1ce0e17effa24214a4d8b73efa3067b2ddade
현재 <77735005+hyenje@users.noreply.github.com>
2024-07-04 18:34:33 +0900

    [Bronze III] Title: Attendance, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/30552. Attendance/Attendance.cc
@@ -0,0 +1,54 @@
+/*
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define xll __int128
+#define F first
+#define S second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define LL_INF 9'223'372'036'854'775'807
+#define INF 2'147'483'647
+#define l_INF 1'000'000'007
+#define MINF -2'147'483'648
+#define MOD 998'244'353
+const int mx[8] = {1, -1, 0, 0, 1, 1, -1, -1};
+const int my[8] = {0, 0, -1, 1, 1, -1, 1, -1};
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<string> arr;
+    string s;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s;
+        if (s == "Present!")
+            arr.pop_back();
+        else
+            arr.push_back(s);
+    }
+
+    for (auto i : arr)
+        cout << i << '\n';
+    if (arr.empty())
+        cout << "No Absences\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    solve();
+
+    return 0;
+}
+/*
+ */
