73b26012eb7abbd239f05b14e13a2a9a4777ce00
현재 <77735005+hyenje@users.noreply.github.com>
2024-09-04 19:07:36 +0900

    [Gold V] Title: 콘센트, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Gold/23843. 콘센트/콘센트.cc
@@ -0,0 +1,51 @@
+/*
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define xll __int128
+#define F first
+#define S second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define LL_INF 9'223'372'036'854'775'807
+#define INF 2'147'483'647
+#define l_INF 1'000'000'007
+#define MINF -2'147'483'648
+#define MOD 998'244'353
+const int mx[8] = {1, -1, 0, 0, 1, 1, -1, -1};
+const int my[8] = {0, 0, -1, 1, 1, -1, 1, -1};
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<ll> arr(n), answer(m);
+    for (auto &i : arr)
+        cin >> i;
+    sort(arr.rbegin(), arr.rend());
+
+    for (auto i : arr)
+    {
+        answer[0] += i;
+        sort(answer.begin(), answer.end());
+    }
+
+    cout << answer[m - 1] << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // int t;
+    // cin >> t;
+    // while (t--)
+    solve();
+
+    return 0;
+}
+/*
+ */
