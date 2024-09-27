8c7c5049cc432f4d07bd22ded41e729d85759150
현재 <77735005+hyenje@users.noreply.github.com>
2024-09-28 04:32:49 +0900

    [Silver V] Title: 너의 평점은, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.cc
@@ -0,0 +1,74 @@
+/*
+ */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define ll long long int
+#define xll __int128
+#define F first
+#define S second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define LL_INF 9'223'372'036'854'775'807
+#define INF 2'147'483'647
+#define l_INF 1'000'000'007
+#define MINF -2'147'483'648
+#define MOD 998'244'353
+const int mx[8] = {1, -1, 0, 0, 1, 1, -1, -1};
+const int my[8] = {0, 0, -1, 1, 1, -1, 1, -1};
+
+double score(string a)
+{
+    double val = 0;
+    if (a[0] == 'A')
+        val = 4.0;
+    else if (a[0] == 'B')
+        val = 3.0;
+    else if (a[0] == 'C')
+        val = 2.0;
+    else if (a[0] == 'D')
+        val = 1.0;
+
+    if (a[1] == '+')
+        val += 0.5;
+    return val;
+}
+
+void solve()
+{
+    string s, a;
+    double val, time = 0, answer = 0;
+
+    for (int i = 0; i < 20; i++)
+    {
+        cin >> s >> val >> a;
+        if (a[0] == 'P')
+            continue;
+
+        time += val;
+        answer += score(a) * val;
+    }
+    if (answer > 0.0)
+        printf("%.6lf\n", answer / time);
+    else
+        cout << 0.0000 << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // int t;
+    // cin >> t;
+    // while (t--)
+    solve();
+
+    return 0;
+}
+/*
+ */
