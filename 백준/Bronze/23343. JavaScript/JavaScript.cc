e0d7a1cef8e1d79cb78e8c76b9cc5cfd2f3425f2
현재 <77735005+hyenje@users.noreply.github.com>
2024-11-11 23:36:13 +0900

    [Bronze III] Title: JavaScript, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/23343. JavaScript/JavaScript.cc
@@ -0,0 +1,54 @@
+/*
+ */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define ll long long int
+#define xll __int128
+#define F first
+#define S second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define LL_INF 9'223'372'036'854'775'807
+#define INF 2'147'483'647
+#define l_INF 1'000'000'007
+#define MINF -2'147'483'648
+#define MOD 998'244'353
+const int mx[8] = {1, -1, 0, 0, 1, 1, -1, -1};
+const int my[8] = {0, 0, -1, 1, 1, -1, 1, -1};
+
+int solve()
+{
+    string s, c;
+    cin >> s >> c;
+
+    for (auto i : s)
+        if (i > '9' || i < '0')
+            return 1;
+    for (auto i : c)
+        if (i > '9' || i < '0')
+            return 1;
+
+    cout << stoi(s) - stoi(c) << '\n';
+    return 0;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    if (solve())
+        cout << "NaN";
+
+    return 0;
+}
+/*
+ */
