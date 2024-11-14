1e3b85abbd3097fdcd0c95f96ab91bc9b0ac5918
현재 <77735005+hyenje@users.noreply.github.com>
2024-11-15 00:08:30 +0900

    [Gold V] Title: 박물관 견학, Time: 228 ms, Memory: 8296 KB -BaekjoonHub

---
+++ 백준/Gold/30405. 박물관 견학/박물관 견학.cc
@@ -0,0 +1,56 @@
+/*
+ */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define ll long long int
+#define xll __int128
+#define F first
+#define S second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define LL_INF 9'223'372'036'854'775'807
+#define INF 2'147'483'647
+#define l_INF 1'000'000'007
+#define MINF -2'147'483'648
+#define MOD 998'244'353
+const int mx[8] = {1, -1, 0, 0, 1, 1, -1, -1};
+const int my[8] = {0, 0, -1, 1, 1, -1, 1, -1};
+
+void solve()
+{
+    ll n, m, k;
+    ll answer = 0, a, b;
+    cin >> n >> m;
+    vector<ll> arr;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> k >> a;
+        for (int j = 1; j < k; j++)
+            cin >> b;
+        arr.push_back(a);
+        arr.push_back(b);
+    }
+    sort(arr.begin(), arr.end());
+
+    cout << arr[arr.size() / 2 - 1] << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    solve();
+
+    return 0;
+}
+/*
+ */
