a058e4a77896528c437236e48f6bdcf7da440dc3
현재 <77735005+hyenje@users.noreply.github.com>
2024-11-20 15:37:21 +0900

    [Silver I] Title: 우울한 방학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17392. 우울한 방학/우울한 방학.cc
@@ -0,0 +1,64 @@
+/*
+ */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define ll long long int
+#define xll __int128
+#define F first
+#define S second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define LL_INF 9'223'372'036'854'775'807
+#define INF 2'147'483'647
+#define l_INF 1'000'000'007
+#define MINF -2'147'483'648
+#define MOD 998'244'353
+const int mx[8] = {1, -1, 0, 0, 1, 1, -1, -1};
+const int my[8] = {0, 0, -1, 1, 1, -1, 1, -1};
+
+void solve()
+{
+    ll n, m, cnt = 0, answer = 0, val;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> val;
+        m -= val + 1, cnt++;
+    }
+    vector<ll> num(cnt + 1);
+
+    if (m <= 0)
+    {
+        cout << 0 << '\n';
+        return;
+    }
+
+    for (ll i = 0; i < m; i++)
+        num[i % (cnt + 1)]++;
+    for (ll i = 0; i < cnt + 1; i++)
+    {
+        for (ll j = 1; j <= num[i]; j++)
+            answer += j * j;
+    }
+
+    cout << answer << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(NULL);
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    solve();
+
+    return 0;
+}
+/*
+ */
