0c9638a3156ebc517f0e8414cb9501db6be87c5e
현재 <77735005+hyenje@users.noreply.github.com>
2024-11-20 14:51:38 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,49 @@
+/*
+ */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define ll long long int
+#define xll __int128
+#define F first
+#define S second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define LL_INF 9'223'372'036'854'775'807
+#define INF 2'147'483'647
+#define l_INF 1'000'000'007
+#define MINF -2'147'483'648
+#define MOD 998'244'353
+const int mx[8] = {1, -1, 0, 0, 1, 1, -1, -1};
+const int my[8] = {0, 0, -1, 1, 1, -1, 1, -1};
+
+void solve()
+{
+    string s;
+    map<string, string> m;
+    m["NLCS"] = "North London Collegiate School";
+    m["BHA"] = "Branksome Hall Asia";
+    m["KIS"] = "Korea International School";
+    m["SJA"] = "St. Johnsbury Academy";
+    cin >> s;
+
+    cout << m[s] << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(NULL);
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    solve();
+
+    return 0;
+}
+/*
+ */
