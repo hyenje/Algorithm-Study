d1612aab3ead39aa3eaa7a89e2f11d569982ead5
현재 <77735005+hyenje@users.noreply.github.com>
2025-05-13 19:42:49 +0900

    [Gold V] Title: 평범한 배낭, Time: 16 ms, Memory: 3708 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,31 +1,70 @@
-#include<iostream>
-#include<algorithm>
+
+/*
+ */
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define ll long long int
+#define xll __int128
+#define F first
+#define S second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define LL_INF 9'223'372'036'854'775'807
+#define INF 2'147'483'647
+#define l_INF 1'000'000'007
+#define MINF -2'147'483'648
+#define MOD 998'244'353
+#define MID 200'000
+#define LM_INF 1'000'000'000'000'000
+const int mx[8] = {1, -1, 0, 0, 1, 1, -1, -1};
+const int my[8] = {0, 0, -1, 1, 1, -1, 1, -1};
 
-int N, K;
-int DP[101][100001];
-int W[101];
-int V[101];
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<pll> arr(n);
+    vector<ll> dp(k + 1, 0);
 
-// 점화식 max(DP[i-1][j], DP[i-1][j-W[i]])
+    for (auto &i : arr)
+        cin >> i.F >> i.S;
+
+    for (int i = 0; i < n; i++)
+    {
+        vector<ll> tmp(k + 1);
+        for (int j = 0; j <= k; j++)
+        {
+            if (j < arr[i].F)
+                tmp[j] = dp[j];
+            else
+                tmp[j] = max(dp[j], dp[j - arr[i].F] + arr[i].S);
+        }
+        dp = tmp;
+    }
+
+    // for (auto i : dp)
+    //     cout << i << ' ';
+    // cout << '\n';
+    cout << dp[k] << '\n';
+}
 
 int main()
 {
-	cin >> N >> K;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(NULL);
 
-	for (int i = 1; i <= N; i++)
-		cin >> W[i] >> V[i];
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    solve();
 
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 1; j <= K; j++)
-		{
-			 
-			 if (j - W[i] >= 0) DP[i][j] = max(DP[i - 1][j], DP[i - 1][j - W[i]] + V[i]);
-			 else DP[i][j] = DP[i - 1][j];
-		}
-	}
-
-	cout << DP[N][K];
-
+    return 0;
 }
+/*
+
+*/
