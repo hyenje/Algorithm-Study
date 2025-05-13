a30bedd130a87c2331b21b253eea24381a0d150a
현재 <77735005+hyenje@users.noreply.github.com>
2025-05-13 20:57:54 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 204 ms, Memory: 30068 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -1,7 +1,11 @@
 /*
  */
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
 #define ll long long int
 #define xll __int128
 #define F first
@@ -13,57 +17,53 @@ using namespace std;
 #define l_INF 1'000'000'007
 #define MINF -2'147'483'648
 #define MOD 998'244'353
+#define MID 200'000
+#define LM_INF 1'000'000'000'000'000
 const int mx[8] = {1, -1, 0, 0, 1, 1, -1, -1};
 const int my[8] = {0, 0, -1, 1, 1, -1, 1, -1};
-
-vector<int> arr, answer, indice;
 
 void solve()
 {
     int n;
     cin >> n;
-
-    arr.resize(n);
-    indice.resize(n);
-
+    vector<ll> arr(n), dp, indice(n);
     for (auto &i : arr)
         cin >> i;
-    int last_index = 0;
-    for (int i = 0; i < n; i++)
+
+    dp.push_back(arr[0]);
+    indice[0] = 0;
+
+    for (int i = 1; i < n; i++)
     {
-        auto iter = lower_bound(answer.begin(), answer.end(), arr[i]) - answer.begin();
-        if (iter == answer.size())
-        {
-            answer.push_back(arr[i]);
-            last_index = i;
-        }
+        int idx = lower_bound(dp.begin(), dp.end(), arr[i]) - dp.begin();
+        if (idx == dp.size())
+            dp.push_back(arr[i]);
         else
-            answer[iter] = arr[i];
-        indice[i] = iter;
+            dp[idx] = arr[i];
+        indice[i] = idx;
     }
-    int num = answer.size() - 1;
-    answer.clear();
-    for (int i = last_index; i >= 0; i--)
+
+    int sz = dp.size();
+    dp.clear();
+    for (int i = arr.size() - 1; i >= 0; i--)
     {
-        if (indice[i] == num)
+        if (sz - 1 == indice[i])
         {
-            answer.push_back(arr[i]);
-            num--;
+            dp.push_back(arr[i]);
+            sz--;
         }
     }
-    // for (int i = 0; i < n; i++)
-    //     cout << indice[i] << ' ';
-    // cout << '\n';
-    cout << answer.size() << '\n';
-    for (int i = answer.size() - 1; i >= 0; i--)
-        cout << answer[i] << ' ';
+    cout << dp.size() << '\n';
+    for (int i = dp.size() - 1; i >= 0; i--)
+        cout << dp[i] << ' ';
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     cout.tie(NULL);
+
     // ll t;
     // cin >> t;
     // while (t--)
@@ -72,4 +72,5 @@ int main()
     return 0;
 }
 /*
- */
+
+*/
