59e26ff6aac5c06ab9264e418d8fc14576e96766
현재 <77735005+hyenje@users.noreply.github.com>
2025-07-23 17:21:12 +0900

    [Gold III] Title: Tower, Time: 128 ms, Memory: 8068 KB -BaekjoonHub

---
+++ 백준/Gold/14061. Tower/Tower.cc
@@ -0,0 +1,70 @@
+//
+// Created by 신현재 on 25. 7. 23.
+//
+/*
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+#define ll long long int
+#define xll __int128
+#define F first
+#define S second
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define LL_INF 9'223'372'036'854'775'807
+#define INF 2'147'483'647
+#define l_INF 1'000'000'007
+#define MINF -2'147'483'648
+#define MOD 998'244'353
+#define MID 200'000
+#define LM_INF 1'000'000'000'000'000
+const int mx[8] = {1, -1, 0, 0, 1, 1, -1, -1};
+const int my[8] = {0, 0, -1, 1, 1, -1, 1, -1};
+
+void solve() {
+    int n, m, answer = 0;
+    cin >> n >> m;
+    vector<int> arr(n);
+    for (auto &it: arr) {
+        cin >> it;
+    }
+
+    stack<int> st;
+
+    for (auto &it: arr) {
+        if (it > m) {
+            while (!st.empty()) st.pop();
+            continue;
+        }
+
+        while (!st.empty() && st.top() < it) st.pop();
+        st.push(it);
+        answer = max(answer, (int) st.size());
+    }
+
+    cout << answer << endl;
+}
+
+/*
+ */
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(NULL);
+
+    // ll t;
+    // cin >> t;
+    // while (t--)
+    solve();
+
+    return 0;
+}
+
+/*
+
+*/
