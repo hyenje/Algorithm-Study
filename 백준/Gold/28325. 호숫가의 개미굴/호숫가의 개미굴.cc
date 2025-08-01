47ab3af83aedf1b0431a38a670445dfe5ce2b50e
현재 <77735005+hyenje@users.noreply.github.com>
2025-08-01 15:08:26 +0900

    [Gold IV] Title: 호숫가의 개미굴, Time: 40 ms, Memory: 7884 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.cc
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.cc
@@ -28,35 +28,36 @@ const int my[8] = {0, 0, -1, 1, 1, -1, 1, -1};
 ll dp[250'123][2], n;
 vector<ll> arr;
 
-void go(ll node) {
-    if (node == n)
-        return;
-    go(node + 1);
-
-    dp[node][1] = dp[node + 1][0] + 1;
-    dp[node][0] = max(dp[node + 1][1] + arr[node], dp[node + 1][0] + arr[node]);
-}
-
 void solve() {
     cin >> n;
     arr.resize(n);
+    ll answer = 0;
 
     for (ll i = 0; i < n; i++)
         cin >> arr[i];
-    arr.push_back(arr[0]);
-    arr.push_back(arr[1]);
 
     dp[0][0] = arr[0];
-    dp[0][1] = 1;
-    for (ll i = 1; i < n; i++) {
-        dp[i][1] = dp[i - 1][0] + 1;
-        dp[i][0] = max(dp[i - 1][1] + arr[i], dp[i - 1][0] + arr[i]);
-    }
+    dp[0][1] = 0;
 
-    if (arr[0]) {
-        cout << max(dp[n - 1][0], dp[n - 1][1]) << endl;
-    } else
-        cout << dp[n - 1][0] << endl;
+    for (int i = 1; i < n; i++) {
+        dp[i][0] = max(dp[i - 1][0], dp[i - 1][1]) + arr[i];
+        dp[i][1] = dp[i - 1][0] + 1;
+    }
+    answer = max(answer, max(dp[n - 1][0], dp[n - 1][1]));
+
+    // cout << answer << endl;
+
+    dp[0][0] = 0;
+    dp[0][0] = 1;
+    dp[1][0] = 1 + arr[1];
+    dp[1][1] = arr[1];
+    for (int i = 2; i < n; i++) {
+        dp[i][0] = max(dp[i - 1][0], dp[i - 1][1]) + arr[i];
+        dp[i][1] = dp[i - 1][0] + 1;
+    }
+    answer = max(answer, dp[n - 1][0]);
+
+    cout << answer << endl;
 }
 
 /*
